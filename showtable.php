f629136a3906c355cc7582be438eace0e4049822
Max <dmxn8a@gmail.com>
2020-05-25 10:46:07 +1200

    new repository

---
+++ showtable.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Show my record</title>
+</head>
+<body>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Username</th>
+        <th>Password</th>
+        <th>Name</th>
+    </tr>
+    <?php $server = "zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $user = "rv8kk7ab6a9led76";
+    $pwd = "co5p03pim1xq00me";
+    $database = "s3ykxtpz1s85tbul";
+
+    $connection = new mysqli($server, $user, $pwd, $database);
+    if ($connection->connect_error) {
+        echo $connection->connect_error;
+    } else {
+        echo "connection created";
+    }
+    $sql = "select * from Users";
+    $result = $connection->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo $row['id'] . ['username'] . $row['password'] . $row['name'] . "<br>";
+            echo "</tr>";
+            echo "<td>".$row['id']."</td>";
+            echo "<td>".$row['username']."</td>";
+            echo "<td>".$row['password']."</td>";
+            echo "<td>".$row['name']."</td>";
+            echo "</tr>";
+        }
+    } else {
+        echo "no result in table";
+    }
+    $connection->close();
+    ?>
+</table>
+</body>
+</html>
